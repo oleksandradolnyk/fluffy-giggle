5bf95d459e4fcc6b0c2052d1dec88f5b43b1f1d9
Oleksandra Dolnyk <oleksandra.dolnyk@gmail.com>
2022-11-09 23:43:08 +0100

    07/11. Lesson 9. Collections, HashSet

---
+++ src/main/java/Homework8_08_11/Set_words.java
@@ -0,0 +1,25 @@
+package Homework8_08_11;
+
+import java.util.*;
+
+public class Set_words {
+
+    public static void main(String[] args) {
+
+    String words = "масло, яйця, масло, ковбаса, молоко, курка, молоко, яйця, молоко, масло, пельмені, курка";
+    strToSet(words);
+
+}
+
+    public static void strToSet(String string){
+
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.addAll(Arrays.asList(string.split(", ")));
+
+        for (String y:hashSet) {
+            String capital = y.substring(0, 1).toUpperCase() + y.substring(1).toLowerCase();
+            System.out.println(capital); // output: Ковбаса Молоко Курка Масло Яйця Пельмені
+        }
+
+    }
+}
